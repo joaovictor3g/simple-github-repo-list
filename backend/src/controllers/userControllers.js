ff8f4daaea81d4930172119ece74d37a9e271dcd
joaovictor3g <jvdias79797@gmail.com>
2020-04-04 11:52:41 -0300

    controllers of aplication. Initial commit

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+module.exports = {
+    async index(req, res) {
+        const { login } = req.body;
+
+        const response = await axios.get(`https://api.github.com/users/${login}`);
+        
+        return res.json(response.data);
+    },
+
+    async listRepos(req, res) {
+        const { id } = req.params;
+
+        const response = await axios.get(`https://api.github.com/users/${id}/repos`);
+
+        return res.json(response.data);
+    }
+};
